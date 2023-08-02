5d50cf042809dc0b2cbc6d46fbf8d291c230b580
rishikamaheswari <rishikamaheswari@kgpian.iitkgp.ac.in>
2023-08-02 17:12:59 +0530

    Array basics

---
+++ Sorting/selection.cpp
@@ -0,0 +1,9 @@
+//Find minimum element in unsorted array and swap it with element at beginning of unsorted array
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i, n;
+    return 0;
+}
